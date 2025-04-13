0fb3d4dfc379d138bfd894938ccae89bdb77d673
Mohd3 <101576770+Mohd3@users.noreply.github.com>
2025-04-13 23:07:33 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# Database configuration
+DB_CONFIG = {
+    "user": "root",
+    "password": "root",
+    "host": "localhost",
+    "name": "mz_neural"
+}
+
+# API keys configuration
+API_KEYS = {
+    "google_api_key": "YOUR_GOOGLE_API_KEY",
+    "tavily_api_key": "YOUR_TAVILY_API_KEY"
+}
